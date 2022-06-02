a49cfa77fedef2eba512c37fde33cbe0f8762003
Sushant <sushantxt123@gmail.com>
2022-06-02 23:16:15 +0530

    changes

--- admin/manage_quiz/manage_quiz.php
+++ admin/manage_quiz/manage_quiz.php
@@ -216,6 +216,9 @@ body {
   <li class="nav-item">
     <a class="nav-link" data-toggle="tab" href="#menu2">Add Questions</a>
   </li>
+  <li class="nav-item">
+    <a class="nav-link" data-toggle="tab" href="#menu3">Upload You File</a>
+  </li>
 </ul>
 		<!-- Nav tabs ends -->
 
@@ -724,6 +727,39 @@ body {
 
   </div>
   				<!-- add questions pane ends -->
+
+	<div class="container tab-pane container fade" id="menu3">
+        <div class="row">
+            <div class="col-md-12 mt-4">
+
+                <?php
+                if(isset($_SESSION['message']))
+                {
+                    echo "<h4>".$_SESSION['message']."</h4>";
+                    unset($_SESSION['message']);
+                }
+                ?>
+
+                <div class="card">
+                    <div class="card-header">
+                        <h4>How to Import Excel Data into database in PHP</h4>
+                    </div>
+                    <div class="card-body">
+
+                        <form action="code.php" method="POST" enctype="multipart/form-data">
+
+                            <input type="file" name="import_file" class="form-control" />
+                            <button type="submit" name="save_excel_data" class="btn btn-primary mt-3">Import</button>
+
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
   <!-- ========================================================================================================================== -->
 </div>
